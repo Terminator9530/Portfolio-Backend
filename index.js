078a3624f56370fec60fd399df830f2e4190c4c1
Vaibhav Shukla <shuklavaibhav0306@gmail.com>
2022-04-15 13:32:00 +0530

    updated procfile and cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors({
-    origin : "http://localhost:3000",
+    origin : ["http://localhost:3000","https://peppy-lamington-626a1e.netlify.app/"],
     credentials : true
 }));
 app.use(express.json());
